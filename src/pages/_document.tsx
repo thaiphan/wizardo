b1507cb79cadcd156f5dc7f8f647b303ede8a748
Thai Phan <thai@outlook.com>
2020-06-06 22:29:41 +1000

    Set up styled-components SSR

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,30 +1,56 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
 import { getLanguages } from '../api';
+import { ServerStyleSheet } from 'styled-components';
 
 interface MyDocumentProps {
   language: string;
 }
 
-export default class extends Document<MyDocumentProps> {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
+export default class MyDocument extends Document<MyDocumentProps> {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-    let language = 'en';
-    if (ctx.query.slug) {
-      const response = await getLanguages();
-      if (
-        response.data.languages.some(
-          (language) => language.id === ctx.query.slug[0]
-        )
-      ) {
-        language = ctx.query.slug[0];
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+
+      let language = 'en';
+      if (ctx.query.slug) {
+        const response = await getLanguages();
+        if (
+          response.data.languages.some(
+            (language) => language.id === ctx.query.slug[0]
+          )
+        ) {
+          language = ctx.query.slug[0];
+        }
       }
-    }
 
-    return {
-      ...initialProps,
-      language,
-    };
+      return {
+        ...initialProps,
+        language,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
